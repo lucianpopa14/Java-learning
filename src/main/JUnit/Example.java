1bb2d65cfb811f55f59dad705da028f3e08cd96a
lucian popa <popaluci1998@gmail.com>
2024-04-23 17:38:51 +0300

    Added Maven support and a test class example

---
+++ src/main/JUnit/Example.java
@@ -0,0 +1,12 @@
+package src.main.JUnit;
+
+public class Example {
+    public int isPositive(int n) {
+        if (n > 0) {
+            return 1;
+        } else if (n==0){
+            return -1;
+        }
+        return 0;
+    }
+}
