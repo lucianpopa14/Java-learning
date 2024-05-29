858ee97338f9a8c5e5f32cb190a7b98463fb1d5f
lucian popa <popaluci1998@gmail.com>
2024-05-29 20:26:06 +0300

    strings exercises

---
+++ src/main/mooc/part3/AgeOfTheOldest.java
@@ -0,0 +1,26 @@
+package src.main.mooc.part3;
+
+import java.util.Scanner;
+
+public class AgeOfTheOldest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int maxAge = 0;
+        String nameOfTheOldest = "";
+        while (true) {
+            String input = scanner.nextLine();
+            String[] pieces = input.split(",");
+            if (input.isEmpty()) {
+                break;
+            }
+
+            if (Integer.parseInt(pieces[1]) > maxAge) {
+                maxAge = Integer.parseInt(pieces[1]);
+                nameOfTheOldest = pieces[0];
+            }
+
+        }
+        System.out.println("The age of the oldest is: " + maxAge);
+        System.out.println("The name of the oldest is: "+ nameOfTheOldest);
+    }
+}
---
+++ src/main/mooc/part3/PrintThrice.java
@@ -0,0 +1,15 @@
+package src.main.mooc.part3;
+
+import java.util.Scanner;
+
+public class PrintThrice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give me a word: ");
+        String input = scanner.nextLine();
+        System.out.print(input);
+        System.out.print(input);
+        System.out.print(input);
+    }
+}
