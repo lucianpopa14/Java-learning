0751158487ef673725d8c3361262100215a0cfd3
lucian popa <popaluci1998@gmail.com>
2024-06-24 23:50:33 +0300

    using more methods

--- src/main/mooc/part4/PaymentCard.java
+++ src/main/mooc/part4/PaymentCard.java
@@ -10,21 +10,46 @@ public class PaymentCard {
     public String toString() {
         return "The card has a balance of " + balance + " euros";
     }
-    public void eatAffordably(){
-        balance-=2.6;
+
+    public void eatAffordably() {
+        if (balance-2.6 >= 0) {
+            balance -= 2.6;
+        }
     }
-    public void eatHeartily(){
-        balance-=4.6;
+
+    public void eatHeartily() {
+        if (balance-4.6 >= 0) {
+            balance -= 4.6;
+        }
     }
+    public void addMoney(double amount){
+        if(balance + amount >= 150){
+            balance=150;
+        }else if(amount<0) {
+        } else{
+            balance+=amount;
+        }
+    }
+//    main here just for testing purposes
+
     public static void main(String[] args) {
-        PaymentCard card = new PaymentCard(50);
-        System.out.println(card);
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
 
-        card.eatAffordably();
-        System.out.println(card);
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+        System.out.println("Paul: "+paulsCard);
+        System.out.println("Matt: "+mattsCard);
 
-        card.eatHeartily();
-        card.eatAffordably();
-        System.out.println(card);
+        paulsCard.addMoney(20);
+        mattsCard.eatHeartily();
+        System.out.println("Paul: "+paulsCard);
+        System.out.println("Matt: "+mattsCard);
+
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        mattsCard.addMoney(50);
+        System.out.println("Paul: "+paulsCard);
+        System.out.println("Matt: "+mattsCard);
     }
 }
