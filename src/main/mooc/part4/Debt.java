3a20d8817831420cff2e1732cb1ccfaf3c4775a8
lucian popa <popaluci1998@gmail.com>
2024-06-25 22:08:10 +0300

    using lists

--- src/main/mooc/part4/Debt.java
+++ src/main/mooc/part4/Debt.java
@@ -15,4 +15,20 @@ public class Debt {
     public void waitOneYear(){
         balance = balance*interestRate;
     }
+    public static void main(String[] args) {
+        Debt mortgage = new Debt(120000.0, 1.2);
+        mortgage.printBalance();
+
+        mortgage.waitOneYear();
+        mortgage.printBalance();
+
+        int years = 0;
+
+        while (years < 20) {
+            mortgage.waitOneYear();
+            years = years + 1;
+        }
+
+        mortgage.printBalance();
+    }
 }
