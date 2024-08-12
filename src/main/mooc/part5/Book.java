d6f4e8a08ac24b03080967eec4522cd8e158e921
lucian popa <popaluci1998@gmail.com>
2024-08-12 15:15:02 +0300

    creating basic classes

---
+++ src/main/mooc/part5/Book.java
@@ -0,0 +1,34 @@
+package src.main.mooc.part5;
+
+public class Book {
+    private String author;
+    private String title;
+    private int pageCount;
+
+    public Book(String author, String title, int pages) {
+        this.author = author;
+        this.title = title;
+        this.pageCount = pages;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public String getName() {
+        return this.title;
+    }
+
+    public int getPageCount() {
+        return this.pageCount;
+    }
+
+    public String toString() {
+        return getAuthor()+", "+getName()+", "+getPageCount();
+    }
+
+    public static void main(String[] args) {
+        Book b = new Book("J. K. Rowling", "Harry Potter", 341);
+        System.out.println(b);
+    }
+}
