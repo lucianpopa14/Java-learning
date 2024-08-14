ee68df5421c18307b2933e1c50c12dd0287f7b44
lucian popa <popaluci1998@gmail.com>
2024-08-14 09:59:13 +0300

    overloading constructors and methods

---
+++ src/main/mooc/part5/Product.java
@@ -0,0 +1,40 @@
+package src.main.mooc.part5;
+
+public class Product {
+    private String name;
+    private String location;
+    private int weight;
+
+    public Product(String name, String location, int weight) {
+        this.name = name;
+        this.location = location;
+        this.weight = weight;
+    }
+
+    // Overloaded constructors
+    public Product(String name) {
+        this(name, "shelf", 1);
+    }
+    public Product(String name, String location) {
+        this(name, location, 1);
+    }
+    public Product(String name, int weight) {
+        this(name, "shelf", weight);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + weight + " kg) can be found from the " + location;
+    }
+
+    public static void main(String[] args) {
+        Product tapeMeasure = new Product("Tape measure");
+        Product plaster = new Product("Plaster", "home improvement section");
+        Product tyre = new Product("Tyre", 5);
+
+        System.out.println(tapeMeasure);
+        System.out.println(plaster);
+        System.out.println(tyre);
+    }
+
+}
