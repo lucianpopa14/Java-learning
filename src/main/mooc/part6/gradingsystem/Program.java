142bd09c074aa4bc1c837bebbf51da6e20581e21
lucian popa <popaluci1998@gmail.com>
2024-09-19 12:01:48 +0300

    Separating the user interface from program logic

---
+++ src/main/mooc/part6/gradingsystem/Program.java
@@ -0,0 +1,14 @@
+package src.main.mooc.part6.gradingsystem;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        GradeRegister register = new GradeRegister();
+
+        UserInterface userInterface = new UserInterface(register, scanner);
+        userInterface.start();
+    }
+}
