887d46dfa626cdc5b345c1581c123869defdcbd6
lucian popa <popaluci1998@gmail.com>
2024-10-14 17:03:58 +0300

    starting part 8 with recap and HashMaps

---
+++ src/main/mooc/part8/Nicknames.java
@@ -0,0 +1,14 @@
+package src.main.mooc.part8;
+
+import java.util.HashMap;
+
+public class Nicknames {
+    public static void main(String[] args) {
+        HashMap<String, String> nicknames = new HashMap<>();
+        nicknames.put("matthew", "matt");
+        nicknames.put("michael", "mix");
+        nicknames.put("arthur","artie");
+
+        System.out.println(nicknames.get("matthew"));
+    }
+}
