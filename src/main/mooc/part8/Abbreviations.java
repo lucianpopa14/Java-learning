2355a2377f57149d4a147f455027d50bfb953105
lucian popa <popaluci1998@gmail.com>
2024-10-15 10:01:09 +0300

    Hashmap exercises

---
+++ src/main/mooc/part8/Abbreviations.java
@@ -0,0 +1,47 @@
+package src.main.mooc.part8;
+
+import java.util.HashMap;
+
+public class Abbreviations {
+    private String abbreviation;
+    private String explanation;
+    HashMap<String, String> abbreviations;
+
+    public Abbreviations() {
+        this.abbreviations = new HashMap<>();
+    }
+
+    public void addAbbreviation(String abbreviation, String explanation) {
+        abbreviations.put(abbreviation, explanation);
+    }
+
+    public boolean hasAbbreviation(String abbreviation) {
+        return abbreviations.containsKey(abbreviation);
+    }
+
+    public String findExplanationFor(String abbreviation) {
+        return abbreviations.getOrDefault(abbreviation, null);
+    }
+
+    //main here for testing purposes
+    public static void main(String[] args) {
+        Abbreviations abbreviations = new Abbreviations();
+        abbreviations.addAbbreviation("e.g.", "for example");
+        abbreviations.addAbbreviation("etc.", "and so on");
+        abbreviations.addAbbreviation("i.e.", "more precisely");
+
+        String text = "e.g. i.e. etc. lol";
+
+        for (String part : text.split(" ")) {
+            if (abbreviations.hasAbbreviation(part)) {
+                part = abbreviations.findExplanationFor(part);
+            }
+
+            System.out.print(part);
+            System.out.print(" ");
+        }
+
+        System.out.println();
+    }
+
+}
