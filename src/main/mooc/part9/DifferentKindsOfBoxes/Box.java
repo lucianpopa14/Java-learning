7159042d5adda52b5476c65c21994a8da8390b79
lucian popa <popaluci1998@gmail.com>
2024-10-24 12:13:37 +0300

    abstract class implementation

---
+++ src/main/mooc/part9/DifferentKindsOfBoxes/Box.java
@@ -0,0 +1,15 @@
+package src.main.mooc.part9.DifferentKindsOfBoxes;
+
+import java.util.ArrayList;
+
+public abstract class Box {
+    public abstract void add(Item item);
+
+    public void add(ArrayList<Item> items) {
+        for (Item item : items) {
+            Box.this.add(item);
+        }
+    }
+
+    public abstract boolean isInBox(Item item);
+}
