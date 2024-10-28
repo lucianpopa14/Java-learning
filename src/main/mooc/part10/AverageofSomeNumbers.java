e279bf9f69cb402c870628a0d8b9b8406ec1655d
lucian popa <popaluci1998@gmail.com>
2024-10-28 11:53:49 +0200

    handling collections as streams

---
+++ src/main/mooc/part10/AverageofSomeNumbers.java
@@ -0,0 +1,42 @@
+package src.main.mooc.part10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AverageofSomeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+
+        System.out.println("Input numbers, type end to stop:");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            } else {
+                list.add(input);
+            }
+        }
+        double averageOfPositives = list.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .filter(number -> number > 0)
+                .average()
+                .getAsDouble();
+
+        double averageOfNegatives = list.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .filter(number -> number < 0)
+                .average()
+                .getAsDouble();
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p):");
+        String posOrNeg = scanner.nextLine();
+
+        if (posOrNeg.toLowerCase().equals("n")) {
+            System.out.println("Average of the negative numbers: " + averageOfNegatives);
+        }
+        if (posOrNeg.toLowerCase().equals("p")) {
+            System.out.println("Average of the positive numbers: " + averageOfPositives);
+        }
+    }
+}
