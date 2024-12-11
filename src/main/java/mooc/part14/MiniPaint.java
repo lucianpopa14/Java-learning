ef6b134afe59cf03869fe20481a3489b415fc69e
lucian popa <popaluci1998@gmail.com>
2024-12-11 09:39:55 +0200

    Created a mini Paint app

---
+++ src/main/java/mooc/part14/MiniPaint.java
@@ -0,0 +1,41 @@
+package mooc.part14;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MiniPaint extends Application {
+
+    @Override
+    public void start(Stage window) {
+
+        Canvas paintingCanvas = new Canvas(640, 480);
+        GraphicsContext painter = paintingCanvas.getGraphicsContext2D();
+
+        ColorPicker colorPalette = new ColorPicker();
+
+        BorderPane paintingLayout = new BorderPane();
+        paintingLayout.setCenter(paintingCanvas);
+        paintingLayout.setRight(colorPalette);
+
+        paintingCanvas.setOnMouseDragged((event) -> {
+            double xLocation = event.getX();
+            double yLocation = event.getY();
+            painter.setFill(colorPalette.getValue());
+            painter.fillOval(xLocation, yLocation, 4, 4);
+        });
+
+        Scene view = new Scene(paintingLayout);
+
+        window.setScene(view);
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch(MiniPaint.class);
+    }
+}
